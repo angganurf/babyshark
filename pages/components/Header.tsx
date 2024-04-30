3683f77831297e615f7c25468d38cbfcf491c024
Angga Nurf <31128877+angganurf@users.noreply.github.com>
2024-04-30 14:13:45 +0700

    Update Header.tsx

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -6,13 +6,13 @@ const Header = () => {
     <Head>
       <title>Inj Baby Shark</title>
       <meta name="Inj Baby Shark" content="Inj.BabyShark" />      
-      <meta name="title" content="Inj Baby Shark">
-      <meta name="description" content="Baby Shark is a community-driven collectibles NFT project. The road map for Baby Shark is collaborative.">
-      <meta name="keywords" content="injective,babyshark,dapps,nft,blockchain,crypto,whitelist,og,early">
-      <meta name="robots" content="index, follow">
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-      <meta name="language" content="English">
-      <meta name="author" content="angganurf">
+      <meta name="title" content="Inj Baby Shark" />
+      <meta name="description" content="Baby Shark is a community-driven collectibles NFT project. The road map for Baby Shark is collaborative." />
+      <meta name="keywords" content="injective,babyshark,dapps,nft,blockchain,crypto,whitelist,og,early" />
+      <meta name="robots" content="index, follow" />
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="language" content="English" />
+      <meta name="author" content="angganurf" />
       {/* favicon */}
       <link rel="icon" href="/shark-fav-black.ico" />
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7659625277371773"
